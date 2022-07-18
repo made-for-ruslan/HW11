705dc1aec81ef2075ef2576f77637c6f73b5a757
made-for-ruslan <107350784+made-for-ruslan@users.noreply.github.com>
2022-07-18 18:20:28 +0300

    chenge

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ try:
     b = input("Введите второе число:")
     result = int(a)/int(b)
 
+
 except ZeroDivisionError:
     a = input("Введите первое число заново:")
     b = input("Введите второе число заново:")
+    result = int(a) / int(b)
 except ValueError:
     print("erorhuerror")
+print(result)
